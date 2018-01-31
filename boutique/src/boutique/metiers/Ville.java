1c7be1690e7ba1ecf283858a26aa46a3044e01d8
manitou <bienvenukengne@gmail>
2018-01-31 16:03:51 +0100

    azre

---
+++ boutique/src/boutique/metiers/Ville.java
@@ -0,0 +1,46 @@
+package boutique.metiers;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="ville")
+@SessionScoped
+public class Ville {
+	
+	private String name;
+	private String cp;
+	
+
+	
+	public Ville() {
+		this.setCp("4000");
+		
+	}
+	public String afficher() {
+		
+		if (getCp().equalsIgnoreCase("44000")) {
+			setName("Nantes");
+		
+			
+		}else {
+			
+		 setName("code postal inconu");
+		}
+		return null;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCp() {
+		return cp;
+	}
+	public void setCp(String cp) {
+		this.cp = cp;
+	}
+	
+
+}
